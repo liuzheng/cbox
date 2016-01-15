d1b93634e472ac54ed3404ba45c7354916ff8bdf
liuzheng712 <liuzheng712@gmail.com>
2016-01-15 15:07:35 +0800

    front-end finish

--- cbox.js
+++ cbox.js
@@ -5,7 +5,7 @@ document.head.appendChild(link);
 
 function makecomments() {
     var contant = {};
-    var textarea = jQuery('textarea');
+    var textarea = jQuery('#editor').parent();
     $.get("https://api.github.com/gists/" + localStorage.getItem(window.location.pathname), function (e) {
         for (var i in e['files']) {
             var res = JSON.parse(e['files'][i]['content']);
@@ -21,44 +21,70 @@ function makecomments() {
         }
     })
 }
+
 $(document).ready(function () {
     var cbox = jQuery('#cbox');
-    var user = cbox.data('gist');
-    var gist = 'https://api.github.com/gists/';
-    var list = 'https://api.github.com/users/' + user + '/gists';
+    var gist = 'https://api.github.com/gists/' + cbox.data('gist');
     var domain = document.domain;
     var domain = 'ilz.me';
-    var comments = document.createElement('p');
 
+    marked.setOptions({
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false
+    });
+    var textarea = jQuery('#editor').parent();
+    var button = document.createElement('button');
+    button.style.float = 'right';
+    button.innerText = '提交';
+    button.onclick = function () {
+        var nick = $('#nick').val();
+        var email = $('#email').val();
+        var text = editor.codemirror.getValue();
+        if (nick && email && text) {
+            if (email.match(/^[a-z]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$/i)) {
+                $.ajax({
+                    type: 'POST',
+                    url: 'url',
+                    data: {'nick': nick, 'email': email, 'text': text},
+                    success: function () {
+                        window.location.reload()
+                    }
+                })
+            }
+        } else {
+            alert('Error input')
+        }
+    };
+    textarea.after(button);
+    var editor = new Editor();
+    editor.render();
 
     if (localStorage.getItem(window.location.pathname)) {
         makecomments()
 
     }
 
-    $.get(list, function (e) {
-        for (var i in e) {
-            for (var j in e[i].files) {
-                if (j == domain) {
-                    var Clist = e[i].files[j]['raw_url'];
-                    break
-                }
+    $.get(gist, function (e) {
+        for (var j in e.files) {
+            if (j == domain) {
+                var Clist = e.files[j]['raw_url'];
+                break
             }
-            if (Clist)break
         }
-        $.get(Clist, function (e) {
-            var json = JSON.parse(e);
-            for (i in json) {
-                localStorage.setItem(i, json[i]);
-                if (i == window.location.pathname && cbox[0].children.length == 1) {
-                    makecomments()
+        if (Clist)
+            $.get(Clist, function (e) {
+                var json = JSON.parse(e);
+                for (i in json) {
+                    localStorage.setItem(i, json[i]);
+                    if (i == window.location.pathname && $('.gist').length == 0) {
+                        makecomments()
+                    }
                 }
-            }
-        })
+            })
     });
-    var child = document.createElement('div');
-    child.style.position = 'absolute';
-    child.style.width = '20px';
-    child.style.height = '20px';
-    child.style.backgroundColor = 'red';
 });
